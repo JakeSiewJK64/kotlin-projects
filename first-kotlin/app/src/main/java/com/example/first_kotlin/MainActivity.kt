82bc7fe55ad1baf6115f6a53bac434ab51428a38
JakeSiewJK64 <joekanesiew@outlook.com>
2022-03-23 11:19:03 +0800

    update: created add and remove buttons to increment and decrement text

--- first-kotlin/app/src/main/java/com/example/first_kotlin/MainActivity.kt
+++ first-kotlin/app/src/main/java/com/example/first_kotlin/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.first_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var counter = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        counter_text.setText(counter.toString());
+        increment_button.setOnClickListener { x -> counter_text.setText(counter++.toString()) }
+        decrement_button.setOnClickListener { x -> counter_text.setText(counter--.toString()) }
     }
 }
