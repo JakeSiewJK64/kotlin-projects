24821227b81f82e09437a99dcc0dab055b2739a3
JakeSiewJK64 <joekanesiew@outlook.com>
2022-03-29 08:10:23 +0800

    update: removed on createview method as it is useless

--- first-kotlin/app/src/main/java/com/example/first_kotlin/Home.kt
+++ first-kotlin/app/src/main/java/com/example/first_kotlin/Home.kt
@@ -1,28 +1,17 @@
 package com.example.first_kotlin
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class Home : Fragment(R.layout.fragment_home) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.mSnackbarButton.setOnClickListener {
-            Snackbar.make(view, "dad", Snackbar.LENGTH_SHORT)
+            Snackbar.make(view, "This is a snackbar", Snackbar.LENGTH_SHORT)
                 .setAction("OK", View.OnClickListener {
                     println("OK Clicked!")
                 }).show()
