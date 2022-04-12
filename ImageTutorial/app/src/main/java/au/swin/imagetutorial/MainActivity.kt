4f904d26c343b64dd7beed7a0aa4efbe67baaf86
JakeSiewJK64 <joekanesiew@outlook.com>
2022-04-12 15:20:50 +0800

    update: ImageTutorial to try to implement save instance state

--- ImageTutorial/app/src/main/java/au/swin/imagetutorial/MainActivity.kt
+++ ImageTutorial/app/src/main/java/au/swin/imagetutorial/MainActivity.kt
@@ -2,12 +2,29 @@ package au.swin.imagetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("LIFECYCLE", "stopped")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            Log.i(
+                "LIFECYCLE",
+                "M_REVENUE: ${savedInstanceState.getInt("M_REVENUE", 0)}"
+            )
+        } else {
+            Log.i("LIFECYCLE", "saved instance is null")
+        }
 
         val mImageView: ImageView = findViewById(R.id.mImageView)
         var isShowOther = false
@@ -20,5 +37,12 @@ class MainActivity : AppCompatActivity() {
                 mImageView.setImageResource(R.drawable.check)
             }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i("LIFECYCLE", "onSaveInstanceState Called")
+        outState.putInt("M_REVENUE", 1)
+        Log.i("LIFECYCLE", "mstate: ${outState.getInt("M_REVENUE", 0)}")
     }
 }
