a2f303ff6357a873efd76cc7fb076f170c629e75
JakeSiewJK64 <joekanesiew@outlook.com>
2022-05-14 22:19:52 +0800

    update: changed logic for location detail

--- joekanesiewweek7core2/app/src/main/java/au/swin/joekanesiew_week7_core2/LocationDetail.kt
+++ joekanesiewweek7core2/app/src/main/java/au/swin/joekanesiew_week7_core2/LocationDetail.kt
@@ -5,16 +5,18 @@ import android.os.Bundle
 import android.widget.*
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LocationDetail : AppCompatActivity() {
 
     private lateinit var locationImageView: ImageView
     private lateinit var locationRatingRating: RatingBar
-    private lateinit var locationTypeTextView: TextInputEditText
-    private lateinit var locationDateTextView: TextInputEditText
-    private lateinit var locationNameTextView: TextInputEditText
     private lateinit var locationFavorite: CheckBox
     private lateinit var locationLinearLayout: LinearLayout
+
+    private lateinit var locationNameInputEditText: TextInputLayout
+    private lateinit var locationDateInputEditText: TextInputLayout
+    private lateinit var locationTypeInputEditText: TextInputLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +24,18 @@ class LocationDetail : AppCompatActivity() {
 
         locationLinearLayout = findViewById(R.id.locationDetailLayout)
         locationImageView = findViewById(R.id.locationDetailImage)
-        locationTypeTextView = findViewById(R.id.locationDetailType)
-        locationDateTextView = findViewById(R.id.locationDetailDate)
         locationRatingRating = findViewById(R.id.locationDetailRatingBar)
-        locationNameTextView = findViewById(R.id.locationDetailName)
         locationFavorite = findViewById(R.id.favoriteLocationCheckbox)
+
+        locationNameInputEditText = findViewById(R.id.locationNameEditTextLayout)
+        locationDateInputEditText = findViewById(R.id.locationDateEditTextLayout)
+        locationTypeInputEditText = findViewById(R.id.locationTypeEditTextLayout)
 
         intent.getParcelableExtra<Location>("locationData")?.let {
             locationImageView.setImageResource(it.locationImage)
-            locationTypeTextView.setText(it.locationType)
-            locationNameTextView.setText(it.locationName)
-            locationDateTextView.setText(it.locationDate)
+            locationNameInputEditText.editText!!.setText(it.locationName)
+            locationTypeInputEditText.editText!!.setText(it.locationType)
+            locationDateInputEditText.editText!!.setText(it.locationDate)
             locationRatingRating.rating = it.locationRating.toString().toFloat()
             locationFavorite.isChecked = it.favorite
             Snackbar.make(locationLinearLayout, "Loaded ${it.locationName} Successfully!", Snackbar.LENGTH_LONG).setAction("OK") {
@@ -42,7 +45,6 @@ class LocationDetail : AppCompatActivity() {
 
     override fun onBackPressed() {
         super.onBackPressed()
-
 
     }
 }
