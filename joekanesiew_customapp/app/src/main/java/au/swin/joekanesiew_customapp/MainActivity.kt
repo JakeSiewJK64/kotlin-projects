d1014003112113681912653669af175171c3e369
JakeSiewJK64 <joekanesiew@outlook.com>
2022-06-12 17:10:18 +0800

    update: linking application to Firestore

--- joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/MainActivity.kt
+++ joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/MainActivity.kt
@@ -2,10 +2,69 @@ package au.swin.joekanesiew_customapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var db: FirebaseFirestore
+    private lateinit var recipeList: ArrayList<Recipe>
+
+    private lateinit var submitButton: Button
+    private lateinit var recipeNameInput: TextInputLayout
+    private lateinit var recipeDescInput: TextInputLayout
+    private lateinit var recipeStepsInput: TextInputLayout
+
+    private fun getRecipeData() {
+        recipeList = ArrayList()
+        db = FirebaseFirestore.getInstance()
+        db.collection("joekanesiew-recipe").addSnapshotListener { value, err ->
+            if (err != null) {
+                Log.e("ERROR", err.message.toString())
+            }
+
+            for (dc: DocumentChange in value?.documentChanges!!) {
+                recipeList.add(dc.document.toObject(Recipe::class.java))
+            }
+            Log.i("DATA", recipeList.toString())
+        }
+    }
+
+    private fun insertData(recipe: Recipe) {
+        val dbCollection = db.collection("joekanesiew-recipe")
+        val docId = dbCollection.document().id
+        dbCollection.document(docId).set(recipe).addOnSuccessListener {
+            Snackbar.make(
+                findViewById(android.R.id.content),
+                "Successfully added new recipe!",
+                Snackbar.LENGTH_LONG
+            ).setAction("OK") {}.show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        submitButton = findViewById(R.id.recipeSubmit)
+        recipeNameInput = findViewById(R.id.recipeNameInput)
+        recipeDescInput = findViewById(R.id.recipeDescInput)
+        recipeStepsInput = findViewById(R.id.recipeStepsInput)
+
+        submitButton.setOnClickListener {
+            insertData(
+                Recipe(
+                    recipeNameInput.editText?.text.toString(),
+                    recipeDescInput.editText?.text.toString(),
+                    recipeStepsInput.editText?.text.toString()
+                )
+            )
+        }
+
+        getRecipeData()
     }
 }
---
+++ joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/Recipe.kt
@@ -0,0 +1,7 @@
+package au.swin.joekanesiew_customapp
+
+data class Recipe(
+    val RecipeName: String? = "",
+    val RecipeDescription: String? = "",
+    val RecipeSteps: String? = ""
+)
