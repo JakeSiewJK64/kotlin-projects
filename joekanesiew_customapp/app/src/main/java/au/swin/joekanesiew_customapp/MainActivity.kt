2174323c47f0f0b9d4e867a6d5abea5060ee207d
JakeSiewJK64 <joekanesiew@outlook.com>
2022-06-13 08:19:11 +0800

    update: added drawer

--- joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/MainActivity.kt
+++ joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/MainActivity.kt
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.top_app_bar.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +24,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recipeNameInput: TextInputLayout
     private lateinit var recipeDescInput: TextInputLayout
     private lateinit var recipeStepsInput: TextInputLayout
+
+    private lateinit var navView: NavigationView
+    private lateinit var actionBarToggle: ActionBarDrawerToggle
+    private lateinit var drawerLayout: DrawerLayout
 
     private fun getRecipeData() {
         recipeList = ArrayList()
@@ -55,6 +64,33 @@ class MainActivity : AppCompatActivity() {
         recipeNameInput = findViewById(R.id.recipeNameInput)
         recipeDescInput = findViewById(R.id.recipeDescInput)
         recipeStepsInput = findViewById(R.id.recipeStepsInput)
+
+        drawerLayout = findViewById(R.id.recipeDrawerLayout)
+        actionBarToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
+        navView = findViewById(R.id.recipeNavView)
+
+        toolbar.setNavigationOnClickListener {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.close()
+            } else {
+                drawerLayout.openDrawer(navView)
+            }
+        }
+
+        drawerLayout.addDrawerListener(actionBarToggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBarToggle.syncState()
+        navView.setNavigationItemSelectedListener {
+            if (it.itemId == R.id.newRecipe) {
+                Snackbar.make(
+                    findViewById(android.R.id.content),
+                    "Launching Add Recipe",
+                    Snackbar.LENGTH_LONG
+                )
+                    .setAction("OK") {}.show()
+            }
+            true
+        }
 
         findViewById<Button>(R.id.recipeListButton).setOnClickListener {
             val i = Intent(this, RecipeList::class.java)
---
+++ joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/NewRecipeFragment.kt
@@ -0,0 +1,7 @@
+package au.swin.joekanesiew_customapp
+
+import androidx.fragment.app.Fragment
+
+class NewRecipeFragment : Fragment(R.layout.fragment_new_recipe) {
+
+}
