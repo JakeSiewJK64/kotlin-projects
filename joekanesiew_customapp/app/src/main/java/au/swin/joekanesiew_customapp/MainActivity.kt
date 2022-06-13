4e1e87de08e4eaba5fb86e50a0747ca5675fd813
JakeSiewJK64 <joekanesiew@outlook.com>
2022-06-13 08:55:49 +0800

    update: added fragment for adding new recipe

--- joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/MainActivity.kt
+++ joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/MainActivity.kt
@@ -1,16 +1,13 @@
 package au.swin.joekanesiew_customapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.top_app_bar.*
@@ -19,11 +16,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var db: FirebaseFirestore
     private lateinit var recipeList: ArrayList<Recipe>
-
-    private lateinit var submitButton: Button
-    private lateinit var recipeNameInput: TextInputLayout
-    private lateinit var recipeDescInput: TextInputLayout
-    private lateinit var recipeStepsInput: TextInputLayout
 
     private lateinit var navView: NavigationView
     private lateinit var actionBarToggle: ActionBarDrawerToggle
@@ -44,26 +36,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun insertData(recipe: Recipe) {
-        val dbCollection = db.collection("joekanesiew-recipe")
-        val docId = dbCollection.document().id
-        dbCollection.document(docId).set(recipe).addOnSuccessListener {
-            Snackbar.make(
-                findViewById(android.R.id.content),
-                "Successfully added new recipe!",
-                Snackbar.LENGTH_LONG
-            ).setAction("OK") {}.show()
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        submitButton = findViewById(R.id.recipeSubmit)
-        recipeNameInput = findViewById(R.id.recipeNameInput)
-        recipeDescInput = findViewById(R.id.recipeDescInput)
-        recipeStepsInput = findViewById(R.id.recipeStepsInput)
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.recipeFragmentFrame, NewRecipeFragment())
+            commit()
+        }
 
         drawerLayout = findViewById(R.id.recipeDrawerLayout)
         actionBarToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
@@ -91,22 +71,6 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-
-        findViewById<Button>(R.id.recipeListButton).setOnClickListener {
-            val i = Intent(this, RecipeList::class.java)
-            startActivity(i)
-        }
-
-        submitButton.setOnClickListener {
-            insertData(
-                Recipe(
-                    recipeNameInput.editText?.text.toString(),
-                    recipeDescInput.editText?.text.toString(),
-                    recipeStepsInput.editText?.text.toString()
-                )
-            )
-        }
-
         getRecipeData()
     }
 }
--- joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/NewRecipeFragment.kt
+++ joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/NewRecipeFragment.kt
@@ -1,7 +1,52 @@
 package au.swin.joekanesiew_customapp
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.firestore.FirebaseFirestore
 
 class NewRecipeFragment : Fragment(R.layout.fragment_new_recipe) {
 
+    private lateinit var db: FirebaseFirestore
+
+    private lateinit var submitButton: Button
+    private lateinit var recipeNameInput: TextInputLayout
+    private lateinit var recipeDescInput: TextInputLayout
+    private lateinit var recipeStepsInput: TextInputLayout
+
+    private fun insertData(recipe: Recipe, view: View) {
+        db = FirebaseFirestore.getInstance()
+        val dbCollection = db.collection("joekanesiew-recipe")
+        val docId = dbCollection.document().id
+        dbCollection.document(docId).set(recipe).addOnSuccessListener {
+            Snackbar.make(
+                view.findViewById(R.id.recipeFragmentLayout),
+                "Successfully added new recipe!",
+                Snackbar.LENGTH_LONG
+            ).setAction("OK") {}.show()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        submitButton = view.findViewById(R.id.recipeSubmit)
+        recipeNameInput = view.findViewById(R.id.recipeNameInput)
+        recipeDescInput = view.findViewById(R.id.recipeDescInput)
+        recipeStepsInput = view.findViewById(R.id.recipeStepsInput)
+
+        submitButton.setOnClickListener {
+            insertData(
+                Recipe(
+                    recipeNameInput.editText?.text.toString(),
+                    recipeDescInput.editText?.text.toString(),
+                    recipeStepsInput.editText?.text.toString()
+                ),
+                view
+            )
+        }
+    }
 }
