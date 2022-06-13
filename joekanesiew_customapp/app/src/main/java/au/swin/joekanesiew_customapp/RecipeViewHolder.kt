ac50eaf8e4cb98c6b3e2bee8de6ec72e30406213
JakeSiewJK64 <joekanesiew@outlook.com>
2022-06-13 11:20:32 +0800

    update: added button for viewholder

--- joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/RecipeViewHolder.kt
+++ joekanesiew_customapp/app/src/main/java/au/swin/joekanesiew_customapp/RecipeViewHolder.kt
@@ -1,16 +1,23 @@
 package au.swin.joekanesiew_customapp
 
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
-class RecipeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class RecipeViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
     private val recipeName: TextView = view.findViewById(R.id.recipeName)
     private val recipeDescription: TextView = view.findViewById(R.id.recipeDescription)
+    private val viewRecipeButton: Button = view.findViewById(R.id.viewRecipeButton)
 
     fun bind(recipe: Recipe) {
         recipeName.text = recipe.RecipeName
         recipeDescription.text = recipe.RecipeDescription
+        viewRecipeButton.setOnClickListener {
+            Snackbar.make(view, "${recipe.RecipeName} Clicked!", Snackbar.LENGTH_LONG)
+                .setAction("OK") {}.show()
+        }
     }
 }
