e776e09b0a56f34ab587e60fc56c113bdba84bd1
JakeSiewJK64 <joekanesiew@outlook.com>
2022-06-18 09:41:43 +0800

    update: used scope function in country details activity

--- joekanesiew_core3/app/src/main/java/au/swin/joekanesiew_core3/MedalistDetailsActivity.kt
+++ joekanesiew_core3/app/src/main/java/au/swin/joekanesiew_core3/MedalistDetailsActivity.kt
@@ -10,24 +10,26 @@ class MedalistDetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_medalist_history)
 
         // retrieve last clicked medalist from shared preferences
-        val sharedPref = getSharedPreferences(MedalistContants.MEDALLISTS_SHARE_PREFS, MODE_PRIVATE)
+        val medalistSharePrefs =
+            getSharedPreferences(MedalistContants.MEDALLISTS_SHARE_PREFS, MODE_PRIVATE)
         val medalistTextView: TextView = findViewById(R.id.medalistDetailsName)
 
         // retrieves the values from share pref file.
-        val shareName: String =
-            sharedPref.getString("[DATA]:PREVIOUS_MEDALIST_NAME", null).toString()
-        val shareIOC: String = sharedPref.getString("[DATA]:PREVIOUS_MEDALIST_IOC", null).toString()
-
-        // checks if the share pref value is null.
-        // if not null, set the TextViews text to each respective values.
-        var mString = "No countries selected yet!"
-        if (shareIOC != "null" && shareName != "null") {
-            mString = String.format(
-                resources.getString(R.string.medalist_details_name),
-                shareName,
-                shareIOC
-            )
+        medalistSharePrefs.apply {
+            val shareName: String =
+                getString("[DATA]:PREVIOUS_MEDALIST_NAME", null).toString()
+            val shareIOC: String = getString("[DATA]:PREVIOUS_MEDALIST_IOC", null).toString()
+            // checks if the share pref value is null.
+            // if not null, set the TextViews text to each respective values.
+            var mString = "No countries selected yet!"
+            if (shareIOC != "null" && shareName != "null") {
+                mString = String.format(
+                    resources.getString(R.string.medalist_details_name),
+                    shareName,
+                    shareIOC
+                )
+            }
+            medalistTextView.text = mString
         }
-        medalistTextView.text = mString
     }
 }
